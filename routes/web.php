f73952494ad1823b538d087a5707328b69d808c7
=Techbuild <techbuildsupp@gmail.com>
2023-02-17 23:29:26 +0200

    addition of the user authentication  module

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,46 @@ Route::get('/userProfile', function(){
 
 Route::get('/userProfile', [\App\Http\Controllers\UserProfileController::class, 'index']);
 Route::get('/userProfile/{userProfile}', [\App\Http\Controllers\UserProfileController::class, 'show']);
+
+//AAuth9
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['namespace' => 'App\Http\Controllers'], function()
+{
+    /**
+     * Home Routes
+     */
+    Route::get('/', 'HomeController@index')->name('home.index');
+
+    Route::group(['middleware' => ['guest']], function() {
+        /**
+         * Register Routes
+         */
+        Route::get('/register', 'RegisterController@show')->name('register.show');
+        Route::post('/register', 'RegisterController@register')->name('register.perform');
+
+        /**
+         * Login Routes
+         */
+        Route::get('/login', 'LoginController@show')->name('login.show');
+        Route::post('/login', 'LoginController@login')->name('login.perform');
+
+    });
+
+    Route::group(['middleware' => ['auth']], function() {
+        /**
+         * Logout Routes
+         */
+        Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+    });
+});
